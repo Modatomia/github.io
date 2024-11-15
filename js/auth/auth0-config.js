92b5befb0512ccee881be9c46e1885f575df396d
Pilar Caicedo <modatomia@gmail.com>
2024-11-15 18:58:20 -0500

    Simplificada implementación de Auth0

--- js/auth/auth0-config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  clientId: "POytIwtV3rWDTkS4apTJCeMzhVqX8v1e",
-  domain: "dev-ao28w8mm01qxvpqa.us.auth0.com",
-  redirectUri: "https://recursos.modatomia.com/callback",
-  returnTo: "https://recursos.modatomia.com",
-  scope: "openid profile email",
-};
-
-export default config;
