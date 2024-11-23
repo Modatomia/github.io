869ce2fdd0bc74a9f901d835a3dc0be6f5fe9345
Pilar Caicedo <modatomia@gmail.com>
2024-11-23 07:28:49 -0500

    feat: update API endpoint URL

--- js/config.js
+++ js/config.js
@@ -1,8 +1,10 @@
+// js/config.js
 const config = {
   AUTH0: {
     domain: "dev-ao28w8mm01qxvpqa.us.auth0.com",
     clientId: "POytIwtV3rWDTkS4apTJCeMzhVqX8v1e",
     baseURL: window.location.origin,
   },
-  API_URL: "https://modatomia-recursos.vercel.app", // Sin /api al final
+  API_URL:
+    "https://modatomia-recursos-c2oniglkn-pilar-caicedos-projects.vercel.app",
 };
